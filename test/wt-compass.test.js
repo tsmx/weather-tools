d34160bafc9b784a5a34ca2d8bf11baf9ea7030b
tsmx <strs@freenet.de>
2022-02-01 20:08:44 +0100

    Compass directions test completed

--- test/wt-compass.test.js
+++ test/wt-compass.test.js
@@ -12,10 +12,25 @@ describe('weather-tools compass/wind direction test suite', () => {
         { deg: 11.25, dir: 'NNE' },
         { deg: 227, dir: 'SW' },
         { deg: 236.24, dir: 'SW' },
-        { deg: 236.25, dir: 'WSW' }
+        { deg: 236.25, dir: 'WSW' },
+        { deg: 22.5, dir: 'NNE' },
+        { deg: 45, dir: 'NE' },
+        { deg: 67.5, dir: 'ENE' },
+        { deg: 90, dir: 'E' },
+        { deg: 112.5, dir: 'ESE' },
+        { deg: 135, dir: 'SE' },
+        { deg: 157.5, dir: 'SSE' },
+        { deg: 180, dir: 'S' },
+        { deg: 202.5, dir: 'SSW' },
+        { deg: 225, dir: 'SW' },
+        { deg: 247.5, dir: 'WSW' },
+        { deg: 270, dir: 'W' },
+        { deg: 292.5, dir: 'WNW' },
+        { deg: 315, dir: 'NW' },
+        { deg: 337.5, dir: 'NNW' }
     ]
 
-    it('tests dgerees to direction translation', () => {
+    it('tests degrees to compass direction translation', () => {
         for (let { deg, dir } of degreeDirectionRef) {
             expect(wt.degreesToDirection(deg)).toEqual(dir);
         }

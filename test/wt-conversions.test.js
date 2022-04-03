d6b6b11d96052d0978c3c8340c1c342e4e6992ff
tsmx <dev@tsmx.net>
2022-04-03 19:54:24 +0200

    Unit tests fo inch-millimeter conversions, version 1.1.0

--- test/wt-conversions.test.js
+++ test/wt-conversions.test.js
@@ -3,7 +3,7 @@ describe('weather-tools unit conversion test suite', () => {
     const wt = require('../weather-tools');
 
     const round2 = require('./test-helpers').roundToTwo;
-    
+
     const speedRefValues = {
         kmhToMph: {
             kmh: 15.0,
@@ -15,7 +15,7 @@ describe('weather-tools unit conversion test suite', () => {
         }
     }
 
-    const tempRefValues = {
+    const temperatureRefValues = {
         celsiusToFahrenheit: {
             clesius: 21.0,
             fahrenheit: 69.8
@@ -23,6 +23,17 @@ describe('weather-tools unit conversion test suite', () => {
         fahrenheitToCelsius: {
             fahrenheit: 72,
             celsius: 22.222
+        }
+    }
+
+    const lengthRefValues = {
+        inchToMillimeter: {
+            inch: 17,
+            millimeter: 431.8
+        },
+        millimeterToInch: {
+            millimeter: 57,
+            inch: 2.24409
         }
     }
 
@@ -35,13 +46,23 @@ describe('weather-tools unit conversion test suite', () => {
     });
 
     it('tests a celsius-to-fahrenheit conversion', () => {
-        expect(round2(wt.celsiusToFahrenheit(tempRefValues.celsiusToFahrenheit.clesius)))
-            .toEqual(round2(tempRefValues.celsiusToFahrenheit.fahrenheit));
+        expect(round2(wt.celsiusToFahrenheit(temperatureRefValues.celsiusToFahrenheit.clesius)))
+            .toEqual(round2(temperatureRefValues.celsiusToFahrenheit.fahrenheit));
     });
 
     it('tests a fahrenheit-to-celsius conversion', () => {
-        expect(round2(wt.fahrenheitToCelsius(tempRefValues.fahrenheitToCelsius.fahrenheit)))
-            .toEqual(round2(tempRefValues.fahrenheitToCelsius.celsius));
+        expect(round2(wt.fahrenheitToCelsius(temperatureRefValues.fahrenheitToCelsius.fahrenheit)))
+            .toEqual(round2(temperatureRefValues.fahrenheitToCelsius.celsius));
+    });
+
+    it('tests a inch-to-millimeter conversion', () => {
+        expect(round2(wt.inchToMillimeter(lengthRefValues.inchToMillimeter.inch)))
+            .toEqual(round2(lengthRefValues.inchToMillimeter.millimeter));
+    });
+
+    it('tests a millimeter-to-inch conversion', () => {
+        expect(round2(wt.millimeterToInch(lengthRefValues.millimeterToInch.millimeter)))
+            .toEqual(round2(lengthRefValues.millimeterToInch.inch));
     });
 
 });
